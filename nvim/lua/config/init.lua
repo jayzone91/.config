386409b45e50683493d03d952f106bd4c5ec49de
jayzone91 <50213036+jayzone91@users.noreply.github.com>
2023-08-28 10:57:44 +0200

    Update init.lua

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,8 +1,4 @@
-if vim.fn.has("win32") == 1 then
-  require("config.catppuccin")
-else
-  require("config.neosolarized")
-end
+require("config.catppuccin")
 require("config.lsp")
 require("config.treesitter")
 require("config.nvim-tree")
