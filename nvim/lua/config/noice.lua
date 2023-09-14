62d132e8e3aa66a1ae5996fe01be29bde58a5400
jayzone91 <50213036+jayzone91@users.noreply.github.com>
2023-09-14 09:59:40 +0200

    Update noice.lua

--- nvim/lua/config/noice.lua
+++ nvim/lua/config/noice.lua
@@ -4,3 +4,32 @@ require("notify").setup({
   render = "compact",
   timeout = 2500,
 })
+require("noice").setup({
+    lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylizy_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        }
+      }
+})
